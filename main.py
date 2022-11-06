c12b55e3b5c03c3490bec2da14c2ac526ba5d9d1
Felix <febernard@web.de>
2022-11-06 21:55:46 +0100

    rect kann laufen

--- main.py
+++ main.py
@@ -6,9 +6,38 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('Hello World!')
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+fps = 60
+clock = pygame.time.Clock()
+geschw = 3
+x = 200
+y = 150
+
+if __name__ == '__main__':
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        gedrueckt = pygame.key.get_pressed()
+        if gedrueckt[pygame.K_UP]:
+            y -= geschw
+        if gedrueckt[pygame.K_RIGHT]:
+            x += geschw
+        if gedrueckt[pygame.K_DOWN]:
+            y += geschw
+        if gedrueckt[pygame.K_LEFT]:
+            x -= geschw
+        
+        DISPLAYSURF.fill(color=WHITE)
+
+        pygame.draw.rect(DISPLAYSURF, BLACK, (x, y, 20, 20), 40, 0)
+
+        pygame.display.flip()
+        clock.tick(fps)
+
+        pygame.display.update()
