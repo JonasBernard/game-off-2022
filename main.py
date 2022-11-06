646a5ddbce437d1a4df72aa9b9191a4933e3a993
Felix <febernard@web.de>
2022-11-06 22:29:30 +0100

    borders with bug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 pygame.init()
 
 DISPLAYSURF = pygame.display.set_mode((800, 500))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('Éscape')
 
 
 BLACK = (0, 0, 0)
@@ -13,8 +13,28 @@ WHITE = (255, 255, 255)
 fps = 60
 clock = pygame.time.Clock()
 geschw = 3
-x = 200
-y = 150
+player_pos_x = 380
+player_pos_y = 230
+
+
+class Map():
+    def __init__(self) -> None:
+        pass
+
+def draw_map():
+    pygame.draw.rect(DISPLAYSURF, BLACK, (400, 30, 20, 70), 40, 0)
+
+def border(pp, direction):
+    if pp + direction <= 0 or pp + direction >= 800:
+        return False
+    else:
+        return True
+    # if pp + direction <= 400:
+    #     return True
+    # else:
+    #     return False
+
+
 
 if __name__ == '__main__':
     while True:
@@ -25,17 +45,23 @@ if __name__ == '__main__':
 
         gedrueckt = pygame.key.get_pressed()
         if gedrueckt[pygame.K_UP]:
-            y -= geschw
+            if border(player_pos_y, 1):
+                player_pos_y -= geschw
         if gedrueckt[pygame.K_RIGHT]:
-            x += geschw
+            if border(player_pos_x, 1):
+                player_pos_x += geschw
         if gedrueckt[pygame.K_DOWN]:
-            y += geschw
+            if border(player_pos_y, -1):
+                player_pos_y += geschw
         if gedrueckt[pygame.K_LEFT]:
-            x -= geschw
+            if border(player_pos_y, -1):
+                player_pos_x -= geschw
         
         DISPLAYSURF.fill(color=WHITE)
 
-        pygame.draw.rect(DISPLAYSURF, BLACK, (x, y, 20, 20), 40, 0)
+        pygame.draw.rect(DISPLAYSURF, BLACK, (player_pos_x, player_pos_y, 20, 20), 40, 0)
+
+        draw_map()
 
         pygame.display.flip()
         clock.tick(fps)
